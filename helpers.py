c73c4829b47fd22163f760d6880a0db1ddd2e666
Nils46 <46447410+Nils46@users.noreply.github.com>
2019-01-23 18:18:08 +0100

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,10 +1,16 @@
-import csv
-import urllib.request
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask_session import Session
+from passlib.apps import custom_app_context as pwd_context
+from tempfile import mkdtemp
+from datetime import datetime
+import os
+import random
 
 from flask import redirect, render_template, request, session
 from functools import wraps
 
-
+db = SQL("sqlite:///database.db")
 
 def apology(message, code=400):
     """Renders message as an apology to user."""
@@ -32,3 +38,24 @@ def login_required(f):
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
+
+
+def categories():
+
+    random.seed(datetime.today().day)
+    print(datetime.today().day)
+    number = random.randint(0,24)
+    print(number)
+
+    categories = ["Cars", "Yachts", "Hotels", "Watches"]
+    random_category = random.choice(categories)
+    random_category_1 = random.choice(categories)
+    url = random_category + ".jpg"
+    url1 = random_category_1 + ".jpg"
+
+    return random_category, random_category_1, url, url1
+
+def names():
+    account = db.execute("SELECT * FROM users WHERE id= :id", id=session["user_id"])
+    name = account[0]["username"]
+    return name
